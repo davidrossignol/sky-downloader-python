7f74f8a291bab7f9ddce80cc3d7a2e15ca45b0bb
David Rossignol <58946203+davidrossignol@users.noreply.github.com>
2019-12-16 11:42:44 -0500

    Added url request
    Added basic url request to pull results from YouTube

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-import os
+import urllib.request
+from bs4 import BeautifulSoup
+
+# Method inspired by Jason Brooks on stack overflow.
+# Link to answer: https://stackoverflow.com/questions/29069444/returning-the-urls-as-a-list-from-a-youtube-search-query
+def askSearch():
+  results = []
+  
+  query = input("Please enter the name of the video you would like to find: ")
+  querypass = urllib.parse.quote(query)
+  url = "https://www.youtube.com/results?search_query=" + querypass
+  res = urllib.request.urlopen(url)
+  html = res.read()
+  soup = BeautifulSoup(html, 'html.parser')
+  for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    results.append(vid)
+    
+def showResults(results):
+  counter = 1
+  for vid in results:
+    print(str(counter) + ". " + vid)
+  
+  
+
+def startProgram():
+  askSearch()
+
+
